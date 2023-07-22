6b548d6b32eefc2ad33f05b3c5ecb11055c13856
Ben Goldfried <2goldenfried@gmail.com>
2023-07-22 23:15:53 +0300

    Add headshot and center items

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-
+import './Home.scss'
 const Home = () => {
   return (
-    <>
+    <div className = 'homeContainer'>
+        <img src={require("../Data/website-picture.png")} alt="headshot" className="headshot" />
         <h1 className="name">Benjamin Goldfried</h1>
-        <h3 className="student">Computer Science Student and Software Engineer</h3>
         <h3 className="major">CS @ Duke University</h3>
-
-        <a href="https://www.linkedin.com/in/benjamin-goldfried/" target="_blank" rel="noopener noreferrer">
-            <img src= {require("../Data/LinkedIn_logo.png")} alt="LinkedIn logo image" />
-        </a>
-        <a href="https://github.com/DukeBen" target="_blank" rel="noopener noreferrer">
-            <img src= {require("../Data/github_logo.png")} alt="GitHub logo image" />
-        </a>
-        <a href="mailto:benjamin.goldfried@duke.edu" target="_blank" rel="noopener noreferrer">
-            <img src = {require("../Data/email.png")} alt="email logo image" />
-        </a>
-    </>
+        <div className="logoContainer">
+            <a href="https://www.linkedin.com/in/benjamin-goldfried/" target="_blank" rel="noopener noreferrer">
+                <i className = "fa fa-linkedin" aria-hidden= 'true'></i>
+            </a>
+            <a href="https://github.com/DukeBen" target="_blank" rel="noopener noreferrer">
+                <i className = "fa fa-github" ></i>
+            </a>
+            <a href="mailto:benjamin.goldfried@duke.edu" target="_blank" rel="noopener noreferrer">
+                <i className = "fa fa-envelope" ></i>
+            </a>
+        </div>
+    </div>
   );
 };
 
