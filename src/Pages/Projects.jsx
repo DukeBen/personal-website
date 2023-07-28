41e2bd3e4a98f216e86b573ad08939a3013c714b
Ben Goldfried <2goldenfried@gmail.com>
2023-07-28 13:08:48 -0400

    Add more pages to website

---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,11 @@
+const Projects = () => {
+    return (
+        <>
+            <div className = 'workContainer' style = {{backgroundColor: 'white', width: '700px', height: '700px'}}>
+                <h1>Projects</h1>
+            </div>
+        </>
+    )
+}
+
+export default Projects;
---
+++ src/Pages/WorkExperience.jsx
@@ -0,0 +1,11 @@
+const WorkExperience = () => {
+    return (
+        <>
+            <div className = 'workContainer' style = {{backgroundColor: 'white', width: '700px', height: '700px'}}>
+                <h1>Work Experience</h1>
+            </div>
+        </>
+    )
+}
+
+export default WorkExperience;
