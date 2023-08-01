264f75757af9076bcac78dde262d41164a6bda8a
Ben Goldfried <2goldenfried@gmail.com>
2023-08-01 15:10:12 -0400

    Add image slider on about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,29 +1,52 @@
-
-
+import "./About.css";
+import ImageSlider from "../Components/ImageSlider";
+import profPic from '../Data/website-picture.png'
+import DukeLogo from '../Data/Duke-Blue-Devils-logo.png'
 const About = () => {
+
+    const slides = [
+        {
+            url: profPic,
+            title: 'profile-pic'
+        },
+        {url: DukeLogo,
+        title: 'Duke-logo'},
+    ]
     return (
-        <>
-            <div className = 'aboutContainer' style = {{backgroundColor: 'white', width: '900px', height: '100vh'}}>
-                <h1>About Me</h1>
-                <p>
-                    Hey! My name is Ben Goldfried.  I am currently a junior at Duke University
-                    studying Computer Science with a minor in Mathematics.  This past summer
-                    I worked as a Software Engineer Intern at a video software startup called 
-                    <a target = '_blank' href='https://www.blings.io/'> Blings.io</a> in Tel Aviv, Israel.
-                    During this internship, I really developed my React.js and Node.js skills, as well as
-                    solidified my interest in creating web applications!  
-                </p>
-                <p>
-                    Outside of Computer Science, my hobbies include reading (just finished Dune!), watching Anime (One Piece and Naruto are goated), lifting weights, hanging out with friends, and traveling.
-                     This this upcoming semester I will be studying abroad in Copenhagen and I could not be more excited to immerse myself
-                     into a new environment and experience a different culture.  
-                </p>
+    <>
+      <div className="aboutContainer">
+        <h1>About Me</h1>
+        <p className="aboutContent" style = {{display: 'flex', justifyContent: 'center'}}>
+          Hey! My name is Ben Goldfried. 
+        </p>
+        <p className='aboutContent'>
+            I am currently a junior at Duke
+          University studying Computer Science with a minor in Mathematics. This
+          past summer I worked as a Software Engineer Intern at a video software
+          startup called
+          <a target="_blank" href="https://www.blings.io/">
+            {" "}
+            Blings.io
+          </a>{" "}
+          in Tel Aviv, Israel. During this internship, I really developed my
+          React.js and Node.js skills, as well as solidified my interest in
+          creating web applications!
+        </p>
+        <p className="aboutContent">
+          Outside of Computer Science, my hobbies include reading (just finished
+          Dune!), watching Anime (One Piece and Naruto are goated), lifting
+          weights, hanging out with friends, and traveling. This this upcoming
+          semester I will be studying abroad in Copenhagen and I could not be
+          more excited to immerse myself into a new environment and experience a
+          different culture.
+        </p>
 
-
-                
-            </div>
-        </>
-    )
-}
+        <div className = 'containerStyles'>
+          <ImageSlider slides={slides} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default About;
