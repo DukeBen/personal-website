df846fc44f39d5fdf5c71786793269db8b06af5e
Ben Goldfried <2goldenfried@gmail.com>
2023-08-03 15:06:37 -0400

    Add more projects and update styling

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -3,9 +3,9 @@ import './ProjectCard.css'
 const ProjectCard = ({title, projectURL, imgURL, text, createdWith}) => {
     return (
         <>
-         <a target="_blank" href = {projectURL} className = 'projectLink'>
-            <div className="projectContainer">
-                <h3 style = {{display: 'flex', justifyContent: "center"}}>{title}</h3>
+         <a target="_blank" href = {projectURL} className = 'projectLink' rel="noopener noreferrer">
+            <div className="projectCard">
+                <h3 style = {{textAlign: 'center'}}>{title}</h3>
                
                     <div className="projectImgContainer">
                         <img src = {imgURL} />
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,38 +1,50 @@
 import "./Projects.css";
 import ProjectCard from "../Components/ProjectCard";
 import etchASketch from "../Data/etch-a-sketch.png";
+import militaryProjImg from "../Data/military-spending-proj.png"
+import websitePreview from "../Data/website-preview.png"
 const Projects = () => {
   const projects = [
+    {
+      title: "Personal Website",
+      projectURL: "https://github.com/DukeBen/personal-website",
+      imgURL: websitePreview,
+      text: 'My personal website.  Made in the summer of 2023 with lots of love <3',
+      createdWith: 'Created with React.js'
+    },
       {
-        title: "Military Spending and Happiness Analysis",
+        title: "Military Spending Analysis",
         projectURL: "https://github.com/DukeBen/miltary-spending-and-happiness",
-        imgURL: '',
-        text: '',
+        imgURL: militaryProjImg,
+        text: 'This was a group project that my friends and I created to investigate a polarizing topic between the left and right',
         createdWith: 'Created with Pandas, NumPy, and Seaborn'
       },
     {
       title: "Etch-A-Sketch",
       projectURL: "https://github.com/DukeBen/etch-a-sketch",
       imgURL: etchASketch,
-      text: "This is one of the first projects I ever made.  Minimalist is part of the design.  Click to find out why!",
+      text: "This is one of the first projects I ever made.  Minimalism is part of the design.  Click to find out why!",
       createdWith: "Created with raw JS, HTML, and CSS",
     },
   ];
   return (
     <>
-      <div className="workContainer">
+      <div className="projectPageContainer">
         <h1>Projects</h1>
+        <div className="projectContainer">
+
         {projects.map((project) => {
           return (
             <ProjectCard
-              title={project.title}
-              projectURL={project.projectURL}
-              imgURL={project.imgURL}
-              text={project.text}
-              createdWith={project.createdWith}
+            title={project.title}
+            projectURL={project.projectURL}
+            imgURL={project.imgURL}
+            text={project.text}
+            createdWith={project.createdWith}
             />
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </>
   );
