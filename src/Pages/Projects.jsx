063460640d36efcd65cfbe054c6ab56d55a20f77
Ben Goldfried <2goldenfried@gmail.com>
2024-07-07 18:11:23 -0500

    Add OpenLook and Enhancing Cancer Detection projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -3,8 +3,24 @@ import ProjectCard from "../Components/ProjectCard";
 import etchASketch from "../Data/etch-a-sketch.png";
 import militaryProjImg from "../Data/military-spending-proj.png"
 import websitePreview from "../Data/website-preview.png"
+import streamingServices from "../Data/streamingServices.png"
+import enhancingCancerDetection from '../Data/medium.png'
 const Projects = () => {
   const projects = [
+    {
+      title: "Enhancing Cancer Detection with StyleGAN-2 ADA",
+      projectURL: "https://github.com/istebbins/Enhancing-Cancer-Detection-with-StyleGAN-2-ADA",
+      imgURL: enhancingCancerDetection,
+      text: "Project published by Towards Data Science.  Uses AI/ML to more accurately detect tumors.",
+      createdWith: "PyTorch, Google Colab, Kaggle"
+    },
+    {
+      title: "OpenLook -- in progress",
+      projectURL: "https://github.com/bmaizes/OpenLook",
+      imgURL: streamingServices,
+      text: "A website that allows you to search for movies/shows based on your streaming platform subscriptions and country.",
+      createdWith: "AWS Lambda, Firebase, Angular"
+    },
     {
       title: "Personal Website",
       projectURL: "https://github.com/DukeBen/personal-website",
@@ -16,7 +32,7 @@ const Projects = () => {
         title: "Military Spending Analysis",
         projectURL: "https://github.com/DukeBen/miltary-spending-and-happiness",
         imgURL: militaryProjImg,
-        text: 'This was a group project that my friends and I created to investigate a topic that frequently makes headlines',
+        text: 'This was a group project that my friends and I created to investigate a topic that frequently makes headlines.',
         createdWith: 'Created with Pandas, NumPy, and Seaborn'
       },
     {
